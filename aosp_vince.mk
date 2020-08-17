271ab372b480372cb9675aa9bf4150d92cc92951
sinchandasgupta <sinchandsgpt9@gmail.com>
2020-08-17 09:37:34 +0000

    vince: aosp-ify

--- aosp_vince.mk
+++ aosp_vince.mk
@@ -1,5 +1,5 @@
 #
-# Copyright (C) 2020 The havocOS Project
+# Copyright (C) 2020 The AOSP Project
 #
 # Licensed under the Apache License, Version 2.0 (the "License");
 # you may not use this file except in compliance with the License.
@@ -21,12 +21,13 @@ $(call inherit-product, $(SRC_TARGET_DIR)/product/full_base_telephony.mk)
 # Inherit from vince device
 $(call inherit-product, device/xiaomi/vince/device.mk)
 
-# Inherit some common havoc stuff.
-$(call inherit-product, vendor/havoc/config/common_full_phone.mk)
+# Inherit some common Evolution-X stuff.
+$(call inherit-product, vendor/aosp/config/common_full_phone.mk)
+$(call inherit-product, device/xiaomi/vince/exos-config.mk)
 
 # Device identifier. This must come after all inclusions
 PRODUCT_DEVICE := vince
-PRODUCT_NAME := havoc_vince
+PRODUCT_NAME := aosp_vince
 PRODUCT_BRAND := Xiaomi
 PRODUCT_MODEL := Redmi 5 Plus
 PRODUCT_MANUFACTURER := Xiaomi
---
+++ exos-config.mk
@@ -0,0 +1,8 @@
+# Face Unlock
+TARGET_FACE_UNLOCK_SUPPORTED := true
+
+# Gapps flags
+TARGET_GAPPS_ARCH := arm64
+
+# Bootanimation
+TARGET_BOOT_ANIMATION_RES := 1080
