99263033819f8e5b76dfd0534eeea2d7806f8882
Sebastiano Barezzi <barezzisebastiano@gmail.com>
2021-01-16 08:53:23 +0100

    vince: Import sensors config
    * From vince-user 8.1.0 OPM1.171019.019 V11.0.2.0.OEGMIXM release-keys
    
    Change-Id: Ie362c8780556dc7f1b624789143f344fc5410572

--- device.mk
+++ device.mk
@@ -291,6 +291,10 @@ PRODUCT_PACKAGES += \
     android.hardware.sensors@1.0-service \
     libsensorndkbridge
 
+# Sensors
+PRODUCT_COPY_FILES += \
+    $(LOCAL_PATH)/configs/sensors/sensor_def_qcomdev.conf:$(TARGET_COPY_OUT_VENDOR)/etc/sensors/sensor_def_qcomdev.conf
+
 # Soong namespaces
 PRODUCT_SOONG_NAMESPACES += \
     $(LOCAL_PATH)
